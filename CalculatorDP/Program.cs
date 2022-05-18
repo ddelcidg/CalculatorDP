52c99f8a465f981653054725072227e8fea8c4cd
ddelcidg <ddelcidg@gmail.com>
2022-05-17 23:45:22 -0600

    Agregar archivos de proyecto.

---
+++ CalculatorDP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculatorDP
+{
+    class Program
+    {   /// <summary>
+        /// Command Design Pattern
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Calculator using Dessign Patters!");
+            User user = new User();
+            // User presses calculator buttons
+            user.Compute('+', 100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+            // Undo 4 commands
+            user.Undo(4);
+            // Redo 3 commands
+            user.Redo(3);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
